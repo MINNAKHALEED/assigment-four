db2127c30450d58610b331bfd92b2f6e9933f4a1
MINNAKHALEED <minnakhaled12@gmail.com>
2024-09-24 18:38:55 +0300

    first commit

---
+++ react-project/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <form>
+    <label htmlFor="email">Email</label>
+    <input type="email" id='email' placeholder='enter your email'/>
+
+    <label htmlFor="password">Password</label>
+    <input type="email" id='password' placeholder='enter your password'/>
+
+    <button type='button'>Submit</button>
+
+    </form>
+  )
+}
+
+export default App
